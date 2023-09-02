09c5a192fd1785be4ac9d88e0398ef3e52e3aeb1
druzxh <badrudin.on@gmail.com>
2023-09-02 23:52:14 +0700

    resize img, logo

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -12,8 +12,8 @@ export default function Sidebar() {
                             <Image
                                 src="https://druzxh.github.io/portfolio-nextjs/assets/logo512.png"
                                 alt="Logo"
-                                width={130}
-                                height={130}
+                                width={80}
+                                height={80}
                             />
                         </Link>
                         <Link href="/">
@@ -50,8 +50,8 @@ export default function Sidebar() {
                         <Image
                             src="https://druzxh.github.io/portfolio-nextjs/assets/profile.png"
                             alt="profile"
-                            height={200}
-                            width={200}
+                            height={150}
+                            width={150}
                             className="object-cover brightness-50 w-full h-full transition-transform duration-300 hover:transform hover:scale-105 hover:translate-y-[-10px] hover:translate-x-[-10px] brightness-100"
                         />
                     </div>
